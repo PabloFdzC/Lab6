7bc411a3af029595af4a99036068ff72906af57e
robertovidal <roberto.vidal.p@hotmail.com>
2022-10-16 17:41:26 -0600

    Cambios en el creador de especimenes

--- CreadorEspecimenes.py
+++ CreadorEspecimenes.py
@@ -1,9 +1,6 @@
 import cv2
 import numpy as np
 
-# Carpeta con especimenes:
-# https://drive.google.com/drive/folders/1qTAt09H2X3dENsdTwr7O5GL1X8xTymgL?usp=sharing
-
 def leer_imagenes(cantidad):
     cantidad_numeros = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for i in range(1, cantidad+1):
@@ -23,7 +20,7 @@ def leer_imagenes(cantidad):
         escala_de_grises = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
         # Se le hace un blur para facilitar la detección de bordes
         blur = cv2.GaussianBlur(escala_de_grises, (5,5), 0)
-        # Se realiza un thresh para que la imagen se vuelva binaria
+        # Se le hace un thresh para que la imagen sea binaria
         thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
         # Se buscan los contornos de la imagen para encontrar los 5
         # rectángulos que contienen los números
@@ -49,9 +46,10 @@ def leer_imagenes(cantidad):
                 cv2.drawContours(mask,[c],0,0,2)
                 out = np.zeros_like(escala_de_grises)
                 out[mask == 255] = escala_de_grises[mask == 255]
+                cv2.imwrite("out.jpg", out)
                 # Se le hace un blur para facilitar la detección de bordes
                 blur = cv2.GaussianBlur(out, (5,5), 0)
-                # Se realiza un thresh para que la imagen se vuelva binaria
+                # Se le hace un thresh para que la imagen sea binaria
                 thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
                 # Se obtienen los contornos de los números dentro de los rectángulos
                 contornos = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -61,13 +59,13 @@ def leer_imagenes(cantidad):
                 num_rectangulo += 1
 
 def crear_especimenes(imagen, escala_de_grises, contornos, numero_imagen, area_total, numero):
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
     # Se recorren los contornos para guardar los especímenes
+    kernel = np.ones((5, 5), np.uint8)
     for i in contornos:
             area = cv2.contourArea(i)
             # Se buscan áreas en este rango debido a que es 
             # el común de los especímenes
-            if area > 1000 and area < 4300:
+            if area > 3000 and area < 4300:
                 x,y,w,h = cv2.boundingRect(i)
                 # Se obtiene el especimen
                 especimen = imagen[y:y+h, x:x+w]
@@ -75,20 +73,21 @@ def crear_especimenes(imagen, escala_de_grises, contornos, numero_imagen, area_t
                 especimen = cv2.cvtColor(especimen, cv2.COLOR_BGR2GRAY)
                 # Se le hace un blur para facilitar la detección de bordes
                 especimen = cv2.GaussianBlur(especimen, (5,5), 0)
-                # Se le realiza una operación morfológica para quitar ruido
-                especimen = cv2.morphologyEx(especimen, cv2.MORPH_CLOSE, kernel, iterations=2)
                 # Se realiza un thresh para que la imagen se vuelva binaria
-                especimen = cv2.adaptiveThreshold(especimen, 255, 1, 1, 11, 2)
+                # el THRESH_OTSU ayuda a que opencv decida cuál es el
+                # umbral más óptimo de manera automática
+                _, especimen = cv2.threshold(especimen, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
                 # Se recorta en el centro para que los especímenes queden del
                 # mismo tamaño
+                especimen = cv2.resize(especimen, (60, 70), interpolation = cv2.INTER_AREA)
                 especimen = especimen[6:66, 6:56]
-                (h, w) = especimen.shape[:2]
-                # Se revisa que realmente quede de
-                # 50x60
-                if h==60 and w ==50:
-                    # Se guarda la imagen en su respectiva carpeta
-                    cv2.imwrite('especimenes/' + str(numero) + '/especimen_' + str(numero_imagen)+ '.jpg', especimen)
-                    numero_imagen += 1
+                especimen = cv2.resize(especimen, (50, 60), interpolation = cv2.INTER_AREA)
+                # Se hace una dilatación de la imagen
+                especimen = cv2.dilate(especimen, kernel, iterations=1)
+                # Se hace una erosión de la imagen
+                especimen = cv2.erode(especimen, kernel, iterations=1)
+                cv2.imwrite('especimenes/' + str(numero) + '/especimen_' + str(numero_imagen)+ '.jpg', especimen)
+                numero_imagen += 1
             # Si el área es mayor quiere decir que se tiene más de un
             # especimen por lo que se debe volver a hacer todo el
             # proceso y aplicar recursividad con los nuevos
@@ -109,4 +108,4 @@ def crear_especimenes(imagen, escala_de_grises, contornos, numero_imagen, area_t
                 numero_imagen = crear_especimenes(imagen, escala_de_grises, contornos_nuevos, numero_imagen, area, numero)
     return numero_imagen
 
-leer_imagenes(22)
+leer_imagenes(28)
